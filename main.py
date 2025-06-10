58f54204001691beacf18447df8a13959446a05e
Andrew Lyons <andrew.r.lyons99@gmail.com>
2025-06-10 18:26:27 -0400

    add post saving to memory

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 # from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -15,29 +16,38 @@ class Post(BaseModel):
     #this is an optional field. If the post request doesnt include a rating, it will not send one. Will throw an error is the value isnt an int.
     rating: Optional[int] = None
 
-#extract data from the body of the payload. Tested using Postman.
-@app.post("/posts")
-def create_posts(new_post: Post):
-    #each post should have title and content as str. Could add other stuff like Bool for draft, type, etc.
-    print(f"Value: {new_post.rating}")
-    #will convert the pydantic class into a dictionary 
-    print(new_post.model_dump())
-    return{"data": new_post}
-    # return{"new_post": f"Title: {new_post.title}. Content: {new_post.content}. Published: {new_post.published}. Rating: {new_post.rating}"}
 
-@app.get("/posts")
-def get_posts():
-    return {"data": "This is your posts."}
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content" : "I like pizza", "id": 2}]
+
 
 #the decorator turns this function into a path/route operation (get HTTP method)
+#extract data from the body of the payload. Tested using Postman.
+@app.post("/posts")
+def create_posts(post: Post):
+    #will convert the pydantic class into a dictionary 
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0,9999999)
+    my_posts.append(post_dict)
+    return {"dada": post_dict}
+    
+
+#get info about all posts
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+#get post by id
 @app.get("/posts{id}")
 def get_posts_by_id():
     pass
 
+#update post by id
+#put needs all fields, patch just needs the field that gets updated.
 @app.put("/posts/{id}")
 def update_posts():
     pass
 
+#delete post by id
 @app.delete("/posts/{id}")
 def delete_posts():
     pass
